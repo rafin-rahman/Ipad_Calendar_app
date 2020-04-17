c2b9a59f75c6a7bdce0856b326adb8da4685af21
Rafin <ra-fin@live.com>
2020-04-17 03:04:35 +0100

    Nativation bar can show / hide

---
+++ Calendars/Navigation Views/EventViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EventViewController.swift
+//  Calendars
+//
+//  Created by Rafin Rahman on 16/04/2020.
+//  Copyright © 2020 Rafin Rahman. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Calendars/Navigation Views/MainViewController.swift
+++ Calendars/Navigation Views/MainViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class StartingViewController: UIViewController {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var rightView: UIView!
     @IBOutlet weak var dynamicView: UIView!
+    @IBOutlet weak var leftView: UIView!
     
     @IBOutlet weak var sidebarWidthConstraint: NSLayoutConstraint!
     
@@ -35,6 +36,19 @@ class StartingViewController: UIViewController {
         })
     }
     
+    @IBAction func onHomeClick(_ sender: UIButton) {
+        rightView.removeFromSuperview()
+        rightView.removeFromSuperview()
+    }
+    
+    
+    @IBAction func onEventClick(_ sender: UIButton) {
+        
+        if let newRightView = UINib(nibName: "EventViewController", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as?
+        EventViewController {
+        setRightViewDetails(newRightView: newRightView)
+        }
+    }
     
     @IBAction func onDayClick(_ sender: Any?) {
         if let newView = UINib(nibName: "DayView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? DayView {
@@ -49,8 +63,15 @@ class StartingViewController: UIViewController {
     }
     @IBAction func onMonthClick(_ sender: UIButton) {
         if let newView = UINib(nibName: "MonthView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? MonthView {
-            setViewDetails(newView: newView)
+        setViewDetails(newView: newView)
         }
+    }
+    
+    @IBAction func onYearClick(_ sender: UIButton) {
+        
+        if let newView = UINib(nibName: "YearView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? YearView {
+               setViewDetails(newView: newView)
+               }
     }
     
     func setViewDetails(newView: UIView) {
@@ -63,6 +84,19 @@ class StartingViewController: UIViewController {
         dynamicView.bottomAnchor.constraint(equalTo: rightView.bottomAnchor).isActive = true
         dynamicView.leadingAnchor.constraint(equalTo: rightView.leadingAnchor).isActive = true
         dynamicView.trailingAnchor.constraint(equalTo: rightView.trailingAnchor).isActive = true
+    }
+    
+    func setRightViewDetails(newRightView: UIView){
+        rightView.removeFromSuperview()
+        rightView = newRightView
+        rightView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(newRightView)
+        rightView.frame = CGRect(x: 0, y: 0, width: rightView.frame.width, height: 1004)
+        rightView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        rightView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        rightView.leadingAnchor.constraint(equalTo: leftView.trailingAnchor).isActive = true
+        rightView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
     }
 
 }
